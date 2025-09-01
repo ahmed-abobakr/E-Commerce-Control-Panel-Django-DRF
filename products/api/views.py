2dfb90808654f2bb612a99b7c36f9927a34fe7b5
ahmed-abobakr <ah.abobakr88@gmail.com>
2025-09-01 13:56:55 +0300

    add pagination to Products and Orders API

--- products/api/views.py
+++ products/api/views.py
@@ -1,13 +1,16 @@
 from rest_framework import viewsets
 from products.models import Product
 from .serializers import ProductSerializer
+from .pagination import ProductPagination
 
 class ProductReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
     
     
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
         
